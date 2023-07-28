160c6c0c4fc0aa8423404873b769f761581eeeaf
Aibibi <88785910+aibiii@users.noreply.github.com>
2023-07-28 11:32:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,6 @@ def policy_page():
 
 
 def support_page():
-    st.subheader("Configuration")
     st.title("BookIT AI App Support")
     st.write(
         "Welcome to our App Support Page. Should you have any queries, require assistance with specific features, or even if you just wish to engage with us, please feel free to contact us at kuanaibibi@gmail.com."
